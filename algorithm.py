2f977553e5c20c6982cbbd518f5482a6aa3dfa73
Matthew van Rijn <matthew.van.rijn@gmail.com>
2017-10-22 14:33:25 +0200

    Latest

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,7 @@ class Reasoner:
     states = []
 
     def __init__(self):
-        start = State(inflow=('0', '0'), outflow=('0', '0'), volume=('0', '0'))
+        start = State(inflow=('0', '0'), outflow=('0', '0'), volume=('0', '0'), pi='-')
         start.id = 0
         self.stack.append(start)
         self.states.append(start)
@@ -16,6 +16,11 @@ class Reasoner:
                 print(state)
 
             new_states = state.get_transitions()
+
+            if not new_states:
+                print('BACKTRACKING')
+            else:
+                print(len(new_states))
 
             for ns in new_states:
                 # Check existing states for equality
@@ -33,20 +38,113 @@ class Reasoner:
 
 
 class State:
-    parents = []
     children = []
     id = None
 
-    def __init__(self, inflow, outflow, volume):
+    def __init__(self, inflow, outflow, volume, pi):
         self.inflow = inflow
         self.outflow = outflow
         self.volume = volume
+        self.previous_inflow = pi
 
     def get_transitions(self):
-        if self.in_dev() == '0':
-            return [State(('0', '+'), self.outflow, self.volume)]
+        states = []
 
-        return []
+        ## Immediate transitions:
+
+        ns = State(self.inflow, self.outflow, self.volume, self.previous_inflow)
+
+        # Inflow
+        if self.in_dev() == '+' and self.in_mag() == '0':
+            ns.inflow = ('+', '+')
+
+        # Volume
+        if self.vol_dev() == '+' and self.vol_mag() == '0':
+            ns.volume = ('+', '+')
+        elif self.vol_dev() == '-' and self.vol_mag() == 'max':
+            ns.volume = ('+', '-')
+
+        # Outflow
+        if self.out_dev() == '+' and self.out_mag() == '0':
+            ns.outflow = ('+', '+')
+        elif self.out_dev() == '-' and self.out_mag() == 'max':
+            ns.outflow = ('+', '-')
+
+        # # VCs
+        # if ns.out_mag() == '0':
+        #     if ns.vol_dev() == '-':
+        #         ns.volume = ('0', '0')
+        #     else:
+        #         ns.volume = ('0', ns.vol_dev())
+        # elif ns.out_mag() == 'max':
+        #     if ns.vol_dev() == '+':
+        #         ns.volume = ('+', '0')
+        #     else:
+        #         ns.volume = ('+', ns.vol_dev())
+        #
+        # if ns.vol_mag() == '0':
+        #     if ns.out_dev() == '-':
+        #         ns.outflow = ('0', '0')
+        #     else:
+        #         ns.outflow = ('0', ns.out_dev())
+        # elif ns.vol_mag() == 'max':
+        #     if ns.out_dev() == '+':
+        #         ns.outflow = ('+', '0')
+        #     else:
+        #         ns.outflow = ('+', ns.out_dev())
+
+        if ns != self:
+            return [ns]
+
+        ## Dependency changes
+
+        # I+ (Inflow, Volume), I- (Outflow, Volume)
+        if (self.in_mag() == '0' and self.out_mag() == '+') or (self.in_mag() == '0' and self.out_mag() == 'max'):
+            if self.vol_dev() != '-' and self.vol_mag() != '0':
+                states.append(State(self.inflow, self.outflow, (self.vol_mag(), '-'), self.previous_inflow))
+        elif self.in_mag() == '+' and self.out_mag() == '0':
+            if self.vol_dev() != '+' and self.vol_mag() != 'max':
+                states.append(State(self.inflow, self.outflow, (self.vol_mag(), '+'), self.previous_inflow))
+        elif (self.in_mag() == '+' and self.out_mag() == '+') or (self.in_mag() == '+' and self.out_mag() == 'max'):
+            if self.vol_dev() != '0':
+                states.append(State(self.inflow, self.outflow, (self.vol_mag(), '0'), self.previous_inflow))
+            else:
+                if self.vol_mag() != '0':
+                    states.append(State(self.inflow, self.outflow, (self.vol_mag(), '-'), self.previous_inflow))
+                if self.vol_mag() != 'max':
+                    states.append(State(self.inflow, self.outflow, (self.vol_mag(), '+'), self.previous_inflow))
+
+        # P+ (Volume, Outflow)
+        for state in states:
+            state.outflow = (state.out_mag(), state.vol_dev())
+
+        if states:
+            return states
+
+        ## Non-immediate Magnitude transitions
+        # Inflow
+        if self.in_dev() == '-' and self.in_mag() == '+':
+            states.append(State(('0', '0'), self.outflow, self.volume, self.previous_inflow))
+
+        # Volume
+        if self.vol_dev() == '+' and self.vol_mag() == '+':
+            states.append(State(self.inflow, self.outflow, ('max', '0'), self.in_dev()))
+        elif self.vol_dev() == '-' and self.vol_mag() == '+':
+            states.append(State(self.inflow, self.outflow, ('0', '0'), self.in_dev()))
+
+        if states:
+            return states
+
+        # Exogenous transitions
+        if self.in_dev() == '0':
+            if self.previous_inflow == '+':
+                states.append(State((self.in_mag(), '-'), self.outflow, self.volume, self.in_dev()))
+            elif self.previous_inflow == '-':
+                states.append(State((self.in_mag(), '+'), self.outflow, self.volume, self.in_dev()))
+        else:
+            states.append(State((self.in_mag(), '0'), self.outflow, self.volume, self.in_dev()))
+
+        return states
 
     def __str__(self):
         out = 'State {}\n'.format(self.id)
@@ -61,12 +159,8 @@ class State:
         if self.inflow != other.inflow or self.outflow != other.outflow or self.volume != other.volume:
             return False
 
-        # Check parent compatibility for exogenous sinusoidal inflow
-        for parent in self.parents:
-            if (other.in_dev() == '-' and parent.in_dev() == '+') or (other.in_dev() == '+' and parent.in_dev() == '-'):
-                return False
-
-        return True
+        # Check compatibility for exogenous sinusoidal inflow
+        return self.previous_inflow == other.previous_inflow
 
     def __ne__(self, other):
         return not self == other
